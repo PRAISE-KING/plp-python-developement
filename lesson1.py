fc2453dc04fdaf71c7f521cb77fce85cda45b766
PRAISE-KING <praiz7king@gmail.com>
2025-03-11 20:43:15 +0300

    first py commit under plp

---
+++ lesson1.py
@@ -0,0 +1,27 @@
+# variable - is a container or storage area for values
+# data types - specify the type of data installed inside a variable (int,str,bool,float,complex,set,,,,etc)
+#   numeric - int,float,complex
+#   string - str
+#   sequence - list.tuple.range
+#   mapping - dict
+#   boolean - bool
+#   set - set
+
+num = 12                           # int
+num2 = 12.4                        # float
+name = "PRAISEKING"                # str
+ai_ml = True                       # bool
+
+# num ,num2 ,name ,ai_ml - are the variables
+
+my_list = [num ,num2 ,name ,ai_ml]
+print(f"{my_list} - are my variables")
+
+
+
+# .....................................................LIST........................................................
+
+students = ['praiz','tess','glorian','Eva',3.5,True,3]
+print(students)
+print(students[0:4])
+
