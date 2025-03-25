1fda4fc5eef883f16e478fbcdb0de53bf0decbd4
PRAISE-KING <praiz7king@gmail.com>
2025-03-25 19:46:16 +0300

    corrected

---
+++ lesson3.1.py
@@ -0,0 +1,27 @@
+# Control flow in python
+
+# there are three form of if statements
+        # if statement
+        
+temp = 23
+if temp >= 23:
+    print("It's a hot day")
+
+
+        # if else statement
+        
+laziness = True
+hunger = False 
+
+if laziness > hunger:
+    print('Continue chilling')
+else:
+    print('Make food')
+        
+        # if elif else statement
+        
+temp2 = 30
+if temp2 >= 20:
+    print("It's a hot day")
+else:
+    print('cold day')  
